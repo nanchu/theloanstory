52a34327626601ec5a3c877bba42f8b83c186cca
Nan Chu <optimuschu@gmail.com>
2017-08-21 01:07:05 -0700

    building out basic tabbing interaction on loan process page and layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
 
 var className = $('body').attr('class');
 
-console.log(className);
-
 $('.'+className+'').addClass('active');
+
+
+
+$(".tab").click(function(){
+	$(this).addClass('active').siblings().removeClass('active');
+	$(".tab-content").removeClass("active");
+	var tabClass = $(this).attr('id');
+	$('.'+tabClass+'').addClass('active');
+
+});
 
 });
