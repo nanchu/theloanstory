3db15ec44e0c488b40b91bc5807c8b566103e06c
Nan Chu <optimuschu@gmail.com>
2017-08-31 19:41:15 -0700

    starting tools tab

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,11 +4,9 @@ var className = $('body').attr('class');
 
 $('.'+className+'').addClass('active');
 
-
-
 $(".tab").click(function(){
 	$(this).addClass('active').siblings().removeClass('active');
-	$(".tab-content").removeClass("active");
+	$('.tab-content').removeClass('active');
 	var tabClass = $(this).attr('id');
 	$('.'+tabClass+'').addClass('active');
 
